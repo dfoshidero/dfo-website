e6592656af7b7f44e4b2c73c5f95ae3352102b70
Favour <dfoshidero@outlook.com>
2023-10-10 23:48:37 +0100

    Remove main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.scss';
-import sass from 'sass';
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import Main from './components/pages/Main';
+import "./App.scss";
+import sass from "sass";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Main from "./components/pages/Main";
 
 function App() {
   return (
@@ -12,8 +12,9 @@ function App() {
       <Routes>
         <Route path="/" component={Main} />
       </Routes>
+      <p className="footerMessage" >Website Build In Progress.</p>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
