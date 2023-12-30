a5c39e65ece42ef8f8550e092456eacfc9e97f2d
Daniel-Favour <136315043+dfoshidero@users.noreply.github.com>
2023-12-30 20:00:54 +0000

    Updated styles and added projects

--- src/content/experience/Experience.jsx
+++ src/content/experience/Experience.jsx
@@ -4,62 +4,160 @@ import { ModalContext } from '../../utils/modalContext';
 
 import './Experience.scss';
 
+import logoKTC from '../../assets/images/logos/ktc.png'; 
+import logoDATERN from '../../assets/images/logos/datern.png'; 
+import logoSkillsCoCreator from '../../assets/images/logos/university_of_bath.png'; 
+import logoACSChair from '../../assets/images/logos/university_of_bath_acs.png'; 
+import logoFP from '../../assets/images/logos/f_p.png'; 
+import logoRS from '../../assets/images/logos/rs_architects_logo.png'; 
+import logoAHMM from '../../assets/images/logos/ahmm.png'; 
+
 const experiences = [
   {
     id: 1,
     title: "Graduate Data Analyst // KTC",
     location: "Wolverhampton, England, United Kingdom",
     shortdesc: "Migration of BI data from CSV to an automated SQL database, and automation of Sales Reporting Dashboard for reducion of manual errors and improved reporting efficiency.",
-    longdesc: "..."
-    // More details can be added here if needed
+    longdesc: `<p>KTC (Edibles) is one of the UK's largest providers of edible oils, canned groceries, and condiments.</p>
+
+    <p>In my role as a Data Analyst at KTC, I conducted the migration of the company's Business Insight data from a local CSV environment to an automated SQL database, as well as simultaneously enhancing their overall Sales Reporting Dashboard. The aim was to eliminate manual processes to minimise human errors, and ensure daily automatic data updates for accurate reporting. Further changes were implemented as needed to improve dashboard usability and flexibility, introducing features such as financial rebate adjustments or contract management. My responsibilities here included:</p>
+    
+    <ul>
+        <li>Participating in Master Data meetings to discuss database status and requirements.</li>
+        <li>Producing project proposals to outline future requirements for assets.</li>
+        <li>Conducting data modelling to identify and document the existing database relationships.</li>
+        <li>Identifying and rectifying data anomalies or duplicates to improve data quality/accuracy.</li>
+        <li>Establishing optimal data architecture for a Business Insight database in the company's SQL environment.</li>
+        <li>Developing and defining scripts to validate and update pending sale data against invoices.</li>
+        <li>Development of automation scripts for data input from Access CRM into the new database.</li>
+        <li>Scheduling automated jobs to execute data scripts at staggered intervals from midnight.</li>
+        <li>Documentation of locally-stored versus system-stored data.</li>
+        <li>Compiling a Data Dictionary detailing data attributes, locations, and the responsible personnel for data handling in the case of potential future changes.</li>
+        <li>Designing an updated dynamic sales BI dashboard for advanced reporting.</li>
+        <li>Development and implementation of Excel VBA scripts for end-user data input to SQL database.</li>
+        <li>Implementation of change auditing for end-user input forms.</li>
+        <li>Implementing Row Level Security features to control dashboard access based on individual role/responsibilities.</li>
+    </ul>
+     `,
+     skills: "Analytical Skills · Interpreting Data · Databases · Visual Basic for Applications (VBA) · Microsoft Excel · Microsoft Power BI · SQL",
+    logo: logoKTC
   },
   {
     id: 2,
     title: "Data Science Intern // Datern",
     location: "Remote",
     shortdesc: "Datern programme included SQL, PowerBI, and Python for Data Analysis. Contributed through data insight extractions, data cleaning and processing, impactful visualizations, utilisation of web scraping and machine learning techniques, and active participation in team meetings and discussions.",
-    longdesc: "..."
-    // More details can be added here if needed
+    longdesc: `<p>As a Data Science Intern at Datern, a company specializing in data science training, I was immersed in an environment dedicated to developing the foundational skills of data analysis. The internship provided a practical and comprehensive understanding of various tools and techniques essential in the field of data science. My training and responsibilities included:</p>
+
+    <ul>
+        <li>Utilizing SQL, PowerBI, and Python for advanced data analysis tasks.</li>
+        <li>Extracting meaningful insights from large and complex datasets.</li>
+        <li>Performing data cleaning and preprocessing to enhance data quality and integrity.</li>
+        <li>Applying web scraping techniques to collect data from various online sources.</li>
+        <li>Implementing machine learning algorithms to analyze data trends and patterns.</li>
+        <li>Creating intuitive and insightful data visualizations and presentations using PowerBI.</li>
+        <li>Actively contributing to team meetings and discussions, offering data-driven perspectives and solutions.</li>
+    </ul>
+    `,
+    skills: "Analytical Skills · Interpreting Data · Databases · Python (Programming Language) · Microsoft Power BI · SQL",
+    logo: logoDATERN
   },
   {
     id: 3,
     title: "Skills Co-Creator & Senior Ambassador // University of Bath",
     location: "Bath, England, United Kingdom | Hybrid",
     shortdesc: "Collaborated on the improvement and promotion of resources for the University's Skill Centre. Largely focused on program design, content review, and video creation on various topics.",
-    longdesc: "..."
-    // More details can be added here if needed
+    longdesc: `<p>As a Skills Co-Creator and Senior Student Ambassador at the University of Bath's Skill Centre, I was part of a dynamic team dedicated to developing and enhancing skill-building resources for student groups. My role involved in-depth research, creative design, and effective communication to bridge skill gaps among students. Key responsibilities and tasks included:</p>
+
+    <ul>
+        <li>Organizing and assisting with focus groups to collect student feedback on various resources.</li>
+        <li>Writing informative blog posts to highlight the available resources and their benefits, based on personal experiences.</li>
+        <li>Reviewing and enhancing existing content and designs for resources and social media posts to improve student appeal and increase page traffic.</li>
+        <li>Contributing to the creation of engaging video content on diverse topics, including exam preparation and mental health awareness.</li>
+    </ul>
+    `,
+    skills: "Management · Analytical Skills · Interpreting Data · Databases · Constructive Feedback · User Experience Testing · Community Outreach · Research · Communication",
+    logo: logoSkillsCoCreator
   },
   {
     id: 4,
     title: "Chair // University of Bath African-Caribbean Society",
     location: "Bath, England, United Kingdom",
     shortdesc: "Directed the ACS within external and internal contexts, overseeing committee affairs, ensuring member wellbeing, managing finances, organizing events, and representing the student group in marketing and communication concerns.",
-    longdesc: "..."
-    // More details can be added here if needed
+    longdesc: `<p>As the Chair of the University of Bath African & Caribbean Society, I played a pivotal role in overseeing the society's functions, focusing on the education, entertainment, and empowerment of individuals from diverse backgrounds, particularly emphasizing Afro-Caribbean culture. My responsibilities were geared towards enriching student experiences and effectively managing the society's activities and goals. Key duties included:</p>
+
+    <ul>
+        <li>Ensuring the wellbeing of committee members and the society as a whole.</li>
+        <li>Collaborating with the Treasurer for efficient management of society funds and overseeing financial transactions.</li>
+        <li>Liaising with the Student Union and other societies for organizing various events, talks, and outings.</li>
+        <li>Engaging in external communication with other universities and businesses for sponsorships and joint affairs.</li>
+        <li>Distributing workload among the committee members to maintain an efficient workflow and foster a supportive team environment.</li>
+        <li>Representing the society in various forums, handling marketing, and managing communications.</li>
+    </ul>
+    `,
+    skills: "Management · Analytical Skills · Fundraising · Strategic Planning · Event Management · Communication · Team Leadership · External Relations · Community Engagement",
+    logo: logoACSChair
   },
   {
     id: 5,
     title: "Architectural Assistant // Foster + Partners",
     location: "London, England, United Kingdom",
     shortdesc: "Contributed as part of a team responsible for the design of hotel section of a large-scale resort project in the Middle East. Utilized CAD and BIM modelling software, generated 3D renders, and designed distribution diagrams.",
-    longdesc: "..."
-    // More details can be added here if needed
+    longdesc: `<p>At Foster + Partners, a globally recognized architectural and engineering practice known for sustainable commercial design, I served as an Architectural Assistant. My role encompassed various stages of architectural design, from conceptualization to schematic design, specifically focusing on the hotel section of a major resort project in the Middle East. My duties were diverse, emphasizing both creative and technical aspects of architectural design. Responsibilities included:</p>
+
+    <ul>
+        <li>Working with CAD and Revit BIM modelling software to create detailed plans and 3D models.</li>
+        <li>Designing room and circulation layouts in accordance with specified area programs.</li>
+        <li>Optimizing atrium designs to enhance natural lighting for planting and ground-floor areas.</li>
+        <li>Producing high-quality renders and visuals using Enscape.</li>
+        <li>Organizing and exporting architectural plans, elevations, and sections in Revit.</li>
+        <li>Creating artistic and realistic rendered plans using Adobe Photoshop.</li>
+        <li>Developing distribution diagrams and compiling reports using Adobe InDesign.</li>
+        <li>Constructing hand-made physical models for client meetings and presentations.</li>
+        <li>Participating in various coordination meetings including design, visualization, and structural discussions.</li>
+    </ul>
+    `,
+    skills: "Adobe Photoshop · Adobe InDesign · Adobe Illustrator · Autodesk AutoCAD 2022 · Autodesk Revit 2022 · Rhinoceros 3D V7 · Enscape · Analytical Skills",
+    logo: logoFP
   },
   {
     id: 6,
     title: "Architectural Assistant // RS Architects",
     location: "Barnet, England, United Kingdom",
     shortdesc: "Handled tasks ranging production of sketches, CAD development, and planning submission for residential projects. Further undertook general office management responsibilities.",
-    longdesc: "..."
-    // More details can be added here if needed
+    longdesc: `<p>At RS Architects, a contemporary multi-disciplinary architectural practice specializing in residential and commercial projects in Greater London, I functioned as an Architectural Assistant. My role entailed close collaboration with the director on various architectural projects including extensions, conversions, and refurbishments of residential buildings. My responsibilities encompassed a range of creative and technical tasks, including:</p>
+
+    <ul>
+        <li>Creating high-quality sketches, renderings, and graphics for client presentations.</li>
+        <li>Producing detailed CAD drawings of existing buildings using surveyed measurements.</li>
+        <li>Managing the cataloguing of drawings for planning submissions, and preparing planning letters and drawing registers.</li>
+        <li>Overseeing the completion of planning application submissions and liaising with authorities for statutory approval documentation.</li>
+        <li>Collaborating with multidisciplinary team members, including structural and civil engineers, to coordinate architectural projects.</li>
+        <li>Handling office management tasks, responding to web enquiries from potential clients, and organizing client engagement.</li>
+        <li>Using Adobe Photoshop to digitally enhance photography of ongoing or completed projects for the practice’s website.</li>
+    </ul>
+    `,
+    skills: "Analytical Skills · Adobe Photoshop · AutoCAD · Client Engagement · Project Coordination · Planning and Design · Technical Documentation",
+    logo: logoRS
   },
   {
     id: 7,
     title: "RIBA Mentee (Internship) // Allford Hall Monoghan Morris",
     location: "Bristol, England, United Kingdom",
-    shortdesc: "...",
-    longdesc: "..."
-    // More details can be added here if needed
+    shortdesc: "Mentee Future Architect.",
+    longdesc: `<p>As a mentee in the RIBA Future Architects mentoring scheme, a program run in partnership with schools of architecture and practices, I gained invaluable insights into the architectural profession. This opportunity at Allford Hall Monaghan Morris (AHMM) allowed me to understand the practical aspects of working as an architect and to receive guidance on career goals and skill development. My experience included:</p>
+
+    <ul>
+        <li>Being introduced to life in practice and receiving practical advice on working as an architect.</li>
+        <li>Receiving tailored advice and guidance to reflect on my future goals and ambitions in the field.</li>
+        <li>Developing key skills and accessing one-on-one support from my RIBA tutor.</li>
+        <li>Getting the opportunity to view ongoing projects at AHMM and understanding the various stages of design.</li>
+    </ul>
+    
+    <p>For more details, see my <a href='https://www.linkedin.com/in/favourdo/details/experience/1612798006841/single-media-viewer/?profileId=ACoAACmhpvMB9MywAsViJ_T-Bq76dnz12-1Zx6c'>Certification</a>.</p>
+    `,
+    skills: "",
+    logo: logoAHMM
   }
 
   // ... other experiences
@@ -68,22 +166,47 @@ const experiences = [
 export default function ExperienceCard() {
   const { openModal } = useContext(ModalContext);
 
+  const handleExperienceClick = (experience) => {
+    openModal(<ExperienceModalContent experience={experience} />);
+  };
+
+  const ExperienceModalContent = ({ experience }) => (
+    <div className="experience-modal-content">
+      <div className="experience-list-modal">
+        {experiences.map((exp) => (
+          <div
+            key={exp.id}
+            className={`experience-item-modal ${exp.id === experience.id ? 'active' : ''}`}
+            onClick={() => handleExperienceClick(exp)}
+          >
+            {exp.title}
+          </div>
+        ))}
+      </div>
+      <div className="experience-detail-modal">
+        <div className="modal-header">
+          <img src={experience.logo} alt={`${experience.title} logo`} className="company-logo" />
+          <div className="modal-text">
+            <h2>{experience.title}</h2>
+            <h3>{experience.location}</h3>
+          </div>
+        </div>
+        <div className="skills" dangerouslySetInnerHTML={{ __html: experience.skills }} />
+
+        <div dangerouslySetInnerHTML={{ __html: experience.longdesc }} />
+
+      </div>
+    </div>
+  );
+
   return (
-    <div className="experience-container"> {/* New Container */}
+    <div className="experience-container">
       <ul className="experience-list">
-        {experiences.map(exp => (
+        {experiences.map((exp) => (
           <li
             key={exp.id}
             className="experience-item"
-            onClick={() => {
-              openModal(
-                <div className="experience-modal-content">
-                  <h2>{exp.title}</h2>
-                  <h3>{exp.location}</h3>
-                  <p>{exp.longdesc}</p>
-                </div>
-              );
-            }}
+            onClick={() => handleExperienceClick(exp)}
           >
             <div className="experience-title">{exp.title}</div>
             <div className="experience-shortdesc">{exp.shortdesc}</div>
--- src/content/projects/Projects.jsx
+++ src/content/projects/Projects.jsx
@@ -5,24 +5,33 @@ import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 
 import bloomImage from '../../assets/images/project-icons/bloom.png';
 import chip8Image from '../../assets/images/project-icons/chip-8.png';
+import personalWebsite from '../../assets/images/project-icons/personal-website.png';
 
 const projects = [
   {
-    id: 3,
-    title: "CHIP-8 Emulator // Ongoing",
-    description: "This emulator aims to replicate the behaviour of a CHIP-8 machine, allowing users to run and interact with CHIP-8 programs.",
-    stack: "C, SDL2",
-    imageUrl: chip8Image,
-    projectUrl: "https://github.com/dfoshidero/CHIP-8-Emulator",
+    id: 1000,
+    title: "DFVRO | Personal Resume Website",
+    description: "If you're seeing this, you've successfully found my personal hub. Be sure to check it out on both mobile and web.",
+    stack: "JavaScript with React, SCSS, Node.js, AWS for API interactions.",
+    imageUrl: personalWebsite,
+    projectUrl: "https://github.com/dfoshidero/dfo-website",
   },
   {
-    id: 2,
+    id: 3,
     title: "Bloom",
     description: "Bloom is an educational mobile app by Team Plum for University of Bath's CM50109 module. It teaches indoor plant care and offers interactive gameplay with rewards.",
     stack: "JavaScript with React-Native, Expo, Node.js",
     imageUrl: bloomImage,
     projectUrl: "https://github.com/dfoshidero/Bloom",
     videoUrl: "https://www.youtube.com/watch?v=v2pALOEpWOQ"
+  },
+  {
+    id: 2,
+    title: "CHIP-8 Emulator // Ongoing",
+    description: "This emulator aims to replicate the behaviour of a CHIP-8 machine, allowing users to run and interact with CHIP-8 programs.",
+    stack: "C, SDL2",
+    imageUrl: chip8Image,
+    projectUrl: "https://github.com/dfoshidero/CHIP-8-Emulator",
   },
   {
     id: 1,
@@ -33,46 +42,81 @@ const projects = [
   },
 ];
 
-  export default function ProjectsCard() {
-    const [isHovered, setIsHovered] = useState(false);
-  
-    const handleProjectClick = (url, event) => {
-      event.stopPropagation();
-      window.open(url, '_blank');
-    };
-  
-    return (
-      <div className="projects-container">
-        <div className="more-info-text">
-          <span>Click item to see more...</span> 
+export default function ProjectsCard() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleProjectClick = (url, event) => {
+    event.stopPropagation();
+    window.open(url, '_blank');
+  };
+
+  // Extract the special project
+  const specialProject = projects.find(project => project.id === 1000);
+  // Filter out the special project from the main list
+  const otherProjects = projects.filter(project => project.id !== 1000);
+
+  return (
+    <div className="projects-container">
+      {/* Render the special project first, outside the projects list */}
+      {specialProject && (
+        <div className="special-project-item"
+             onClick={(event) => handleProjectClick(specialProject.projectUrl, event)}>
+          <div className="project-image-and-actions">
+            {specialProject.imageUrl && (
+              <div className="project-image-container">
+                <img src={specialProject.imageUrl} alt={specialProject.title} className="project-image" />
+              </div>
+            )}
+            {specialProject.videoUrl && (
+              <a href={specialProject.videoUrl} target="_blank" rel="noopener noreferrer" className="view-button" 
+                 onClick={(event) => { event.stopPropagation(); window.open(specialProject.videoUrl, '_blank'); }} 
+                 onMouseEnter={() => setIsHovered(true)}
+                 onMouseLeave={() => setIsHovered(false)}>
+                Demo <PlayCircleOutlineIcon className="button-icon" />
+              </a>
+            )}
+          </div>
+          <div className="project-details">
+            <div className="project-title">{specialProject.title}</div>
+            <div className="project-description">{specialProject.description}</div>
+            <div className="project-stack">Stack: {specialProject.stack}</div>
+          </div>
         </div>
-        <ul className="projects-list">
-          {projects.map(project => (
-            <li key={project.id} 
-                className={`project-item ${isHovered ? 'no-hover' : ''} ${!project.videoUrl ? 'no-action-button' : ''}`} 
-                onClick={(event) => handleProjectClick(project.projectUrl, event)}>
-              <div className="project-image-and-actions">
-                {project.imageUrl && (
-                    <div className="project-image-container">
-                      <img src={project.imageUrl} alt={project.title} className="project-image" />
-                    </div>
-                )}
-                {project.videoUrl && (
-                  <a href={project.videoUrl} target="_blank" rel="noopener noreferrer" className="view-button" onClick={(event) => { event.stopPropagation(); window.open(project.videoUrl, '_blank'); }} onMouseEnter={() => setIsHovered(true)}
-                     onMouseLeave={() => setIsHovered(false)}>
-                    Demo <PlayCircleOutlineIcon className="button-icon" />
-                  </a>
-                )}
-              </div>
-              <div className="project-details">
-                <div className="project-title">{project.title}</div>
-                <div className="project-description">{project.description}</div>
-                <div className="project-stack">Stack: {project.stack}</div>
-              </div>
-            </li>
-          ))}
-        </ul>
+      )}
+
+      <div className="more-info-text">
+        <span>Click items to see more...</span> 
       </div>
-    );
-  }
-  
+
+      <ul className="projects-list">
+        {/* Render the other projects */}
+        {otherProjects.map(project => (
+          <li key={project.id} 
+              className={`project-item ${isHovered ? 'no-hover' : ''} ${!project.videoUrl ? 'no-action-button' : ''}`} 
+              onClick={(event) => handleProjectClick(project.projectUrl, event)}>
+            <div className="project-image-and-actions">
+              {project.imageUrl && (
+                <div className="project-image-container">
+                  <img src={project.imageUrl} alt={project.title} className="project-image" />
+                </div>
+              )}
+              {project.videoUrl && (
+                <a href={project.videoUrl} target="_blank" rel="noopener noreferrer" className="view-button" 
+                   onClick={(event) => { event.stopPropagation(); window.open(project.videoUrl, '_blank'); }} 
+                   onMouseEnter={() => setIsHovered(true)}
+                   onMouseLeave={() => setIsHovered(false)}>
+                  Demo <PlayCircleOutlineIcon className="button-icon" />
+                </a>
+              )}
+            </div>
+            <div className="project-details">
+              <div className="project-title">{project.title}</div>
+              <div className="project-description">{project.description}</div>
+              <div className="project-stack">Stack: {project.stack}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
