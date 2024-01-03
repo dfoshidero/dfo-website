c14d3b393bdf7b43e8df47b59a028cceaff8a0b0
Daniel-Favour <136315043+dfoshidero@users.noreply.github.com>
2024-01-03 01:19:13 +0000

    updated database sim link

--- src/content/projects/Projects.jsx
+++ src/content/projects/Projects.jsx
@@ -38,7 +38,7 @@ const projects = [
     title: "Airline Database (CRUD) Simulator",
     stack: "Python with Tkinter, SQLite3",
     description: "Basic database simulator developed to explore data management in the context of a real-world system, using CRUD operations.",
-    projectUrl: "https://github.com/dfoshidero/Airline-Database-Sim",
+    projectUrl: "https://github.com/dfoshidero/Database-CRUD-Sim",
   },
 ];
 
