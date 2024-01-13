25d771d7461fee438bb3f51c2bc782decff67bf9
Favour <dfoshidero@outlook.com>
2024-01-13 17:30:25 +0000

    Updated education card styling to highlight degrees

--- src/content/education/Education.jsx
+++ src/content/education/Education.jsx
@@ -4,46 +4,47 @@ import './Education.scss';
 import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 
 const educations = [
-  {
-    id: 5,
-    title: "Master of Science (M.Sc) // Computer Science",
-    location: "Bath, United Kingdom",
-    school: "University of Bath",
-    graduation: "Expected 2024.",
-    achieved: ""
-    // More details can be added here if needed
-  },
-  {
-    id: 4,
-    title: "Udemy // Complete 2023 Web Development Bootcamp",
-    graduation: "Issued Oct 2023",
-    link: "https://www.udemy.com/certificate/UC-f3a9d97a-913d-453e-b471-ed31b2479a4e/"
-  },
-  {
-    id: 3,
-    title: "Amazon Web Services (AWS) // Certified Cloud Practitioner",
-    graduation: "Issued Jul 2023",
-    link: "https://www.credly.com/badges/4c769782-1735-4aae-b652-1413a1e65b75/linked_in_profile"
-  },
-  {
-    id: 2,
-    title: "Bachelor of Science with Honours (B.Sc Hons) // Architecture",
-    location: "Bath, United Kingdom",
-    school: "University of Bath",
-    graduation: "Graduated 2023.",
-    achieved: "2:1"
-    // More details can be added here if needed
-  },
-  {
-    id: 1,
-    title: "Advanced-Level Qualifications // Art, Physcs, Maths",
-    location: "London, United Kingdom",
-    school: "Harlington School",
-    graduation: "Graduated 2019.",
-    achieved: "A*, A, A"
-    // More details can be added here if needed
-  },
-  
+	{
+		id: 5,
+		title: "Master of Science (M.Sc) // Computer Science",
+		location: "Bath, United Kingdom",
+		school: "University of Bath",
+		graduation: "Expected 2024.",
+		achieved: "",
+		type: "degree",
+		// More details can be added here if needed
+	},
+	{
+		id: 4,
+		title: "Udemy // Complete 2023 Web Development Bootcamp",
+		graduation: "Issued Oct 2023",
+		link: "https://www.udemy.com/certificate/UC-f3a9d97a-913d-453e-b471-ed31b2479a4e/",
+	},
+	{
+		id: 3,
+		title: "Amazon Web Services (AWS) // Certified Cloud Practitioner",
+		graduation: "Issued Jul 2023",
+		link: "https://www.credly.com/badges/4c769782-1735-4aae-b652-1413a1e65b75/linked_in_profile",
+	},
+	{
+		id: 2,
+		title: "Bachelor of Science with Honours (B.Sc Hons) // Architecture",
+		location: "Bath, United Kingdom",
+		school: "University of Bath",
+		graduation: "Graduated 2023.",
+		achieved: "2:1",
+		type: "degree",
+		// More details can be added here if needed
+	},
+	{
+		id: 1,
+		title: "Advanced-Level Qualifications // Art, Physcs, Maths",
+		location: "London, United Kingdom",
+		school: "Harlington School",
+		graduation: "Graduated 2019.",
+		achieved: "A*, A, A",
+		// More details can be added here if needed
+	},
 ];
 
 export default function EducationCard() {
@@ -51,7 +52,7 @@ export default function EducationCard() {
     <div className="education-container">
       <ul className="education-list">
         {educations.map(ed => (
-          <li key={ed.id} className="education-item">
+          <li key={ed.id} className={`education-item ${ed.type === 'degree' ? 'degree-item' : ''}`}>
             <div className="education-title">{ed.title}</div>
             <div className="graduation-and-button">
               <div className="education-grad">
--- src/home/LayoutConfigRandom.jsx
+++ src/home/LayoutConfigRandom.jsx
@@ -1,230 +1,320 @@
 // LayoutConfigRandom.js
-import React from 'react';
-import Card from '../components/card/Card';
+import React from "react";
+import Card from "../components/card/Card";
 import ExperienceCard from "../content/experience/Experience";
-import EducationCard from '../content/education/Education';
-import AnalogClock from '../content/time/analog/AnalogClock';
-import ProjectCard from '../content/projects/Projects';
-import StatusCard from '../content/status/Status';
-import PortfolioCard from '../content/portfolio/Portfolio';
-import ContactCard from '../content/contact/Contact';
-import RecommendationCard from '../content/recommendations/Recommendation';
-import SkillsCard from '../content/expertise/Skills';
+import EducationCard from "../content/education/Education";
+import AnalogClock from "../content/time/analog/AnalogClock";
+import ProjectCard from "../content/projects/Projects";
+import StatusCard from "../content/status/Status";
+import PortfolioCard from "../content/portfolio/Portfolio";
+import ContactCard from "../content/contact/Contact";
+import RecommendationCard from "../content/recommendations/Recommendation";
+import SkillsCard from "../content/expertise/Skills";
 
-import StatusCircle from '../content/status/StatusCircle';
-import SeeMore from '../components/seemore-button/SeeMore';
+import StatusCircle from "../content/status/StatusCircle";
+import SeeMore from "../components/seemore-button/SeeMore";
 
 // Define the possible sizes for each card type
 const cardTypes = {
-    'EXPERIENCE': [{ columns: 2, rows: 3 }, { columns: 2, rows: 2 }], // Now an array of sizes
-    'TIMEZONE': [{ columns: 1, rows: 1 }],
-    'CONNECT': [{ columns: 1, rows: 1 }],
-    'PROJECTS': [{ columns: 2, rows: 2 }, { columns: 2, rows: 3 }],
-    'MY WORK(S)': [{ columns: 2, rows: 2 }],
-    'EDUCATION': [{ columns: 2, rows: 1 }, { columns: 1, rows: 2 }, { columns: 2, rows: 2 }],
-    'RECOMMENDATIONS': [{ columns: 2, rows: 1 }],
-    'STATUS': [{ columns: 2, rows: 1 }, { columns: 1, rows: 1 }],
-    'TECHNICAL SKILLS': [{ columns: 1, rows: 2 }, { columns: 1, rows: 1 }], // Will include skills and certifications. CERTIFIED SKILLS go first.
+	EXPERIENCE: [
+		{ columns: 2, rows: 3 },
+		{ columns: 2, rows: 2 },
+	], // Now an array of sizes
+	TIMEZONE: [{ columns: 1, rows: 1 }],
+	CONNECT: [{ columns: 1, rows: 1 }],
+	PROJECTS: [
+		{ columns: 2, rows: 2 },
+		{ columns: 2, rows: 3 },
+	],
+	"MY WORK(S)": [{ columns: 2, rows: 2 }],
+	EDUCATION: [
+		{ columns: 2, rows: 1 },
+		{ columns: 1, rows: 2 },
+		{ columns: 2, rows: 2 },
+	],
+	RECOMMENDATIONS: [{ columns: 2, rows: 1 }],
+	STATUS: [
+		{ columns: 2, rows: 1 },
+		{ columns: 1, rows: 1 },
+	],
+	"TECHNICAL SKILLS": [
+		{ columns: 1, rows: 2 },
+		{ columns: 1, rows: 1 },
+	], // Will include skills and certifications. CERTIFIED SKILLS go first.
 };
 
 // Function to check if the layout has at least two cards from [TIMEZONE, STATUS, CONNECT] in the first three rows for mobile layout
 const isValidSpecialCardsPlacement = (layout) => {
-    const specialCardTypes = ['TIMEZONE', 'STATUS', 'CONNECT'];
-    let count = 0;
+	const specialCardTypes = ["TIMEZONE", "STATUS", "CONNECT"];
+	let count = 0;
 
-    layout.forEach(item => {
-        if (specialCardTypes.includes(item.cardType) && item.position.rowStart <= 3) {
-            count++;
-        }
-    });
+	layout.forEach((item) => {
+		if (
+			specialCardTypes.includes(item.cardType) &&
+			item.position.rowStart <= 3
+		) {
+			count++;
+		}
+	});
 
-    return count >= 1;
+	return count >= 1;
 };
 
 // Function to generate a random layout
 const generateRandomLayout = (gridColumns, gridRows) => {
-    const cardTypeKeys = Object.keys(cardTypes);
-    let layout, grid, attempt, allCardTypesUsed, layoutValid;
+	const cardTypeKeys = Object.keys(cardTypes);
+	let layout, grid, attempt, allCardTypesUsed, layoutValid;
 
-    do {
-        layout = [];
-        grid = Array(gridRows).fill().map(() => Array(gridColumns).fill(false));
-        attempt = true;
-        allCardTypesUsed = true;
+	do {
+		layout = [];
+		grid = Array(gridRows)
+			.fill()
+			.map(() => Array(gridColumns).fill(false));
+		attempt = true;
+		allCardTypesUsed = true;
 
-        for (let row = 1; row <= gridRows && attempt; row++) {
-            for (let col = 1; col <= gridColumns && attempt; col++) {
-                if (!grid[row - 1][col - 1]) {
-                    const availableOptions = getAvailableCardTypesAndSizes(cardTypeKeys, layout, row, col, grid, gridColumns, gridRows);
-                    if (availableOptions.length === 0) {
-                        attempt = false;
-                        allCardTypesUsed = false;
-                    } else {
-                        const { cardType, size } = selectRandomElement(availableOptions);
-                        const position = { columnStart: col, rowStart: row };
+		for (let row = 1; row <= gridRows && attempt; row++) {
+			for (let col = 1; col <= gridColumns && attempt; col++) {
+				if (!grid[row - 1][col - 1]) {
+					const availableOptions = getAvailableCardTypesAndSizes(
+						cardTypeKeys,
+						layout,
+						row,
+						col,
+						grid,
+						gridColumns,
+						gridRows
+					);
+					if (availableOptions.length === 0) {
+						attempt = false;
+						allCardTypesUsed = false;
+					} else {
+						const { cardType, size } = selectRandomElement(availableOptions);
+						const position = { columnStart: col, rowStart: row };
 
-                        layout.push({ cardType, size, position });
-                        updateGrid(grid, position, size);
-                    }
-                }
-            }
-        }
+						layout.push({ cardType, size, position });
+						updateGrid(grid, position, size);
+					}
+				}
+			}
+		}
 
-        // Check if all card types have been used
-        if (allCardTypesUsed) {
-            const usedCardTypes = layout.map(item => item.cardType);
-            allCardTypesUsed = cardTypeKeys.every(cardType => usedCardTypes.includes(cardType));
-        }
+		// Check if all card types have been used
+		if (allCardTypesUsed) {
+			const usedCardTypes = layout.map((item) => item.cardType);
+			allCardTypesUsed = cardTypeKeys.every((cardType) =>
+				usedCardTypes.includes(cardType)
+			);
+		}
 
-        // Additional check for special card placement if gridRows === 12
-        layoutValid = gridRows !== 12 || isValidSpecialCardsPlacement(layout);
+		// Additional check for special card placement if gridRows === 12
+		layoutValid = gridRows !== 12 || isValidSpecialCardsPlacement(layout);
+	} while (!allCardTypesUsed || !layoutValid);
 
-    } while (!allCardTypesUsed || !layoutValid);
-
-    return layout;
+	return layout;
 };
-
 
 // Function to get available card types that can fit in the current position
-const getAvailableCardTypesAndSizes = (cardTypeKeys, layout, row, col, grid, gridColumns, gridRows) => {
-    let availableOptions = [];
-    const usedCardTypes = layout.map(item => item.cardType);
+const getAvailableCardTypesAndSizes = (
+	cardTypeKeys,
+	layout,
+	row,
+	col,
+	grid,
+	gridColumns,
+	gridRows
+) => {
+	let availableOptions = [];
+	const usedCardTypes = layout.map((item) => item.cardType);
 
-    cardTypeKeys.forEach(cardType => {
-        if (!usedCardTypes.includes(cardType)) {
-            cardTypes[cardType].forEach(size => {
-                // Exclude specific card sizes when gridRows is 12
-                if (gridRows === 12 && ((cardType === 'EDUCATION' && size.columns === 2 && size.rows === 2) || (cardType === 'STATUS' && size.columns === 2 && size.rows === 1))) {
-                    return;
-                }
+	cardTypeKeys.forEach((cardType) => {
+		if (!usedCardTypes.includes(cardType)) {
+			cardTypes[cardType].forEach((size) => {
+				// Exclude specific card sizes when gridRows is 12
+				if (
+					gridRows === 12 &&
+					((cardType === "EDUCATION" &&
+						size.columns === 2 &&
+						size.rows === 2) ||
+						(cardType === "STATUS" && size.columns === 2 && size.rows === 1))
+				) {
+					return;
+				}
 
-                if (isValidPosition(size, { columnStart: col, rowStart: row }, layout, grid, gridColumns, gridRows)) {
-                    availableOptions.push({ cardType, size });
-                }
-            });
-        }
-    });
+				if (
+					gridRows === 4 &&
+					((cardType === "EDUCATION" &&
+						size.columns === 1 &&
+						size.rows === 2))
+				) {
+					return;
+				}
 
-    return availableOptions;
+				if (
+					isValidPosition(
+						size,
+						{ columnStart: col, rowStart: row },
+						layout,
+						grid,
+						gridColumns,
+						gridRows
+					)
+				) {
+					availableOptions.push({ cardType, size });
+				}
+			});
+		}
+	});
+
+	return availableOptions;
 };
-
-
 
 // Function to update the grid marking the occupied cells
 const updateGrid = (grid, position, size) => {
-    for (let row = position.rowStart; row < position.rowStart + size.rows; row++) {
-        for (let col = position.columnStart; col < position.columnStart + size.columns; col++) {
-            grid[row - 1][col - 1] = true;
-        }
-    }
+	for (
+		let row = position.rowStart;
+		row < position.rowStart + size.rows;
+		row++
+	) {
+		for (
+			let col = position.columnStart;
+			col < position.columnStart + size.columns;
+			col++
+		) {
+			grid[row - 1][col - 1] = true;
+		}
+	}
 };
 
 // Updated function to check if the position is valid for the given size
-const isValidPosition = (size, position, currentLayout, grid, gridColumns, gridRows) => {
-    const columnEnd = position.columnStart + size.columns - 1;
-    const rowEnd = position.rowStart + size.rows - 1;
+const isValidPosition = (
+	size,
+	position,
+	currentLayout,
+	grid,
+	gridColumns,
+	gridRows
+) => {
+	const columnEnd = position.columnStart + size.columns - 1;
+	const rowEnd = position.rowStart + size.rows - 1;
 
-    // Check if a 2x2 card is being placed in the first row when gridRows is 12
-    if (gridRows === 12 && size.columns === 2 && size.rows === 2 && position.rowStart === 1) {
-        return false;
-    }
+	// Check if a 2x2 card is being placed in the first row when gridRows is 12
+	if (
+		gridRows === 12 &&
+		size.columns === 2 &&
+		size.rows === 2 &&
+		position.rowStart === 1
+	) {
+		return false;
+	}
 
-    if (columnEnd > gridColumns || rowEnd > gridRows) {
-        return false;
-    }
+	if (columnEnd > gridColumns || rowEnd > gridRows) {
+		return false;
+	}
 
-    for (let r = position.rowStart; r <= rowEnd; r++) {
-        for (let c = position.columnStart; c <= columnEnd; c++) {
-            if (grid[r - 1][c - 1]) { // Check if the cell is already occupied
-                return false;
-            }
-        }
-    }
+	for (let r = position.rowStart; r <= rowEnd; r++) {
+		for (let c = position.columnStart; c <= columnEnd; c++) {
+			if (grid[r - 1][c - 1]) {
+				// Check if the cell is already occupied
+				return false;
+			}
+		}
+	}
 
-    return true;
+	return true;
 };
-
 
 // Function to select a random element from an array
 const selectRandomElement = (array) => {
-    return array[Math.floor(Math.random() * array.length)];
+	return array[Math.floor(Math.random() * array.length)];
 };
 
-const allowAutoScroll = ['MY WORK(S)', 'TECHNICAL SKILLS', 'PROJECTS'];
+const allowAutoScroll = ["MY WORK(S)", "TECHNICAL SKILLS", "PROJECTS"];
 
 // Function to generate layout components based on the random layout
 export const generateLayoutComponents = (gridColumns, gridRows) => {
-    let layoutConfigurations = generateRandomLayout(gridColumns, gridRows);
+	let layoutConfigurations = generateRandomLayout(gridColumns, gridRows);
 
-    return layoutConfigurations.map((config, index) => {
-        const cardClasses = `card ${config.size.columns}-columns ${config.size.rows}-rows`;
-        // Define a delay based on the card's index
-        const animationDelay = index * 0.1; // Adjust the multiplier to control the speed
+	return layoutConfigurations.map((config, index) => {
+		const cardClasses = `card ${config.size.columns}-columns ${config.size.rows}-rows`;
+		// Define a delay based on the card's index
+		const animationDelay = index * 0.1; // Adjust the multiplier to control the speed
 
-        let extraContent;
+		let extraContent;
 
-        switch (config.cardType) {
-            case 'TIMEZONE':
-                extraContent = "Midlands, UK";
-                break;
-            case 'STATUS':
-                //lightgreen
-                //orange
-                //red
-                extraContent = <StatusCircle color="lightgreen" />;
-                break;
-            case 'RECOMMENDATIONS':
-                extraContent = <SeeMore url="https://www.linkedin.com/in/favourdo/details/recommendations/?detailScreenTabIndex=0" text="READ MORE" />
-                break;
-            case 'PROJECTS':
-                extraContent = <SeeMore url="https://github.com/dfoshidero?tab=repositories" text="VIEW REPOSITORIES" />
-                break;
-            case 'MY WORK(S)':
-                extraContent = (
-                    <div style={{ display: 'flex', flexDirection: 'row' }}>
-                        <SeeMore
-                            url="https://www.instagram.com/untitled.fvr/"
-                            text="TRADITIONAL"
-                            style={{ marginRight: '10px' }} // Optional margin between components
-                        /> ||
-                        <SeeMore
-                            url="https://www.instagram.com/_farchived/"
-                            text="DIGITAL"
-                        />
-                    </div>
-                );                break;
-            default:
-                extraContent = null;
-        }
+		switch (config.cardType) {
+			case "TIMEZONE":
+				extraContent = "Midlands, UK";
+				break;
+			case "STATUS":
+				//lightgreen
+				//orange
+				//red
+				extraContent = <StatusCircle color="lightgreen" />;
+				break;
+			case "RECOMMENDATIONS":
+				extraContent = (
+					<SeeMore
+						url="https://www.linkedin.com/in/favourdo/details/recommendations/?detailScreenTabIndex=0"
+						text="READ MORE"
+					/>
+				);
+				break;
+			case "PROJECTS":
+				extraContent = (
+					<SeeMore
+						url="https://github.com/dfoshidero?tab=repositories"
+						text="VIEW REPOSITORIES"
+					/>
+				);
+				break;
+			case "MY WORK(S)":
+				extraContent = (
+					<div style={{ display: "flex", flexDirection: "row" }}>
+						<SeeMore
+							url="https://www.instagram.com/untitled.fvr/"
+							text="TRADITIONAL"
+							style={{ marginRight: "10px" }} // Optional margin between components
+						/>{" "}
+						||
+						<SeeMore
+							url="https://www.instagram.com/_farchived/"
+							text="DIGITAL"
+						/>
+					</div>
+				);
+				break;
+			default:
+				extraContent = null;
+		}
 
-        const shouldScroll = allowAutoScroll.includes(config.cardType);
+		const shouldScroll = allowAutoScroll.includes(config.cardType);
 
-        return () => (
-            <Card
-                key={index} // Add a unique key for each card
-                title={config.cardType.toUpperCase()}
-                extra={extraContent}
-                className={cardClasses}
-                style={{
-                    gridColumn: `span ${config.size.columns}`,
-                    gridRow: `span ${config.size.rows}`,
-                    // Apply the animation delay
-                    animationDelay: `${animationDelay}s`,
-                }}
-                scroll={shouldScroll}
-            >
-                {/* Render card content based on cardType */}
-                {config.cardType === 'EXPERIENCE' && <ExperienceCard />}
-                {config.cardType === 'EDUCATION' && <EducationCard />}
-                {config.cardType === 'TIMEZONE' && <AnalogClock />}
-                {config.cardType === 'PROJECTS' && <ProjectCard />}
-                {config.cardType === 'STATUS' && <StatusCard />}
-                {config.cardType === 'MY WORK(S)' && <PortfolioCard />}
-                {config.cardType === 'CONNECT' && <ContactCard />}
-                {config.cardType === 'RECOMMENDATIONS' && <RecommendationCard />}
-                {config.cardType === 'TECHNICAL SKILLS' && <SkillsCard />}
-
-            </Card>
-        );
-    });
+		return () => (
+			<Card
+				key={index} // Add a unique key for each card
+				title={config.cardType.toUpperCase()}
+				extra={extraContent}
+				className={cardClasses}
+				style={{
+					gridColumn: `span ${config.size.columns}`,
+					gridRow: `span ${config.size.rows}`,
+					// Apply the animation delay
+					animationDelay: `${animationDelay}s`,
+				}}
+				scroll={shouldScroll}
+			>
+				{/* Render card content based on cardType */}
+				{config.cardType === "EXPERIENCE" && <ExperienceCard />}
+				{config.cardType === "EDUCATION" && <EducationCard />}
+				{config.cardType === "TIMEZONE" && <AnalogClock />}
+				{config.cardType === "PROJECTS" && <ProjectCard />}
+				{config.cardType === "STATUS" && <StatusCard />}
+				{config.cardType === "MY WORK(S)" && <PortfolioCard />}
+				{config.cardType === "CONNECT" && <ContactCard />}
+				{config.cardType === "RECOMMENDATIONS" && <RecommendationCard />}
+				{config.cardType === "TECHNICAL SKILLS" && <SkillsCard />}
+			</Card>
+		);
+	});
 };
