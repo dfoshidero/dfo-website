88bd665917af53fd54d128dad42c222dd988291c
Favour <dfoshidero@outlook.com>
2024-07-10 12:14:46 +0100

    Removed unused variables

--- src/content/time/analog/AnalogClock.js
+++ src/content/time/analog/AnalogClock.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import "./AnalogClock.scss";
 
 const AnalogClock = () => {
-  const [time, setTime] = useState(new Date());
+  const [, setTime] = useState(new Date());
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -41,29 +41,29 @@ const AnalogClock = () => {
   
   return (
     <div className="clock-container">
-    <div className="clock">
-      {Array.from({ length: 12 }).map((_, index) => (
-        <div
-          key={`hour_${index}`}
-          className="mark hour-mark"
-          style={{ transform: `rotate(${index * 30}deg)` }}
-        >
-          <div className="mark-line" />
-        </div>
-      ))}
-      {Array.from({ length: 60 }).map((_, index) => (
-        <div
-          key={`minute_${index}`}
-          className="mark minute-mark"
-          style={{ transform: `rotate(${index * 6}deg)` }}
-        >
-          <div className="minute-mark-line" />
-        </div>
-      ))}
-      <div className="hand hour-hand" style={{ transform: `rotate(${hourDegrees}deg)` }} />
-      <div className="hand minute-hand" style={{ transform: `rotate(${minuteDegrees}deg)` }} />
-      <div className="hand second-hand" style={{ transform: `rotate(${secondDegrees}deg)` }} />
-    </div>
+      <div className="clock">
+        {Array.from({ length: 12 }).map((_, index) => (
+          <div
+            key={`hour_${index}`}
+            className="mark hour-mark"
+            style={{ transform: `rotate(${index * 30}deg)` }}
+          >
+            <div className="mark-line" />
+          </div>
+        ))}
+        {Array.from({ length: 60 }).map((_, index) => (
+          <div
+            key={`minute_${index}`}
+            className="mark minute-mark"
+            style={{ transform: `rotate(${index * 6}deg)` }}
+          >
+            <div className="minute-mark-line" />
+          </div>
+        ))}
+        <div className="hand hour-hand" style={{ transform: `rotate(${hourDegrees}deg)` }} />
+        <div className="hand minute-hand" style={{ transform: `rotate(${minuteDegrees}deg)` }} />
+        <div className="hand second-hand" style={{ transform: `rotate(${secondDegrees}deg)` }} />
+      </div>
     </div>
   );
 };
